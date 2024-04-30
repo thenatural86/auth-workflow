b1e94b8216d2a5ac6790bd438551f8bc2b33edd7
thenatural86 <philip.smalls86@gmail.com>
2024-04-30 13:29:49 -0300

    login refactor

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -45,6 +45,11 @@ const login = async (req, res) => {
   if (!isPasswordCorrect) {
     throw new CustomError.UnauthenticatedError('Invalid Credentials')
   }
+
+  if (!user.isVerified) {
+    throw new CustomError.UnauthenticatedError('Please verify your email')
+  }
+
   const tokenUser = createTokenUser(user)
   attachCookiesToResponse({ res, user: tokenUser })
 
