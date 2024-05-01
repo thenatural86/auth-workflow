1ed2597c59d286056e6e8bf7bd8220e625e51b73
thenatural86 <philip.smalls86@gmail.com>
2024-05-01 06:56:46 -0300

    adds verify email function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,6 +3,8 @@ const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
 const { attachCookiesToResponse, createTokenUser } = require('../utils')
 
+const crypto = require('crypto')
+
 const register = async (req, res) => {
   const { email, name, password } = req.body
 
@@ -15,7 +17,7 @@ const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments({})) === 0
   const role = isFirstAccount ? 'admin' : 'user'
 
-  const verificationToken = 'Fake token'
+  const verificationToken = crypto.randomBytes(40).toString('hex')
 
   const user = await User.create({
     name,
@@ -63,8 +65,32 @@ const logout = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: 'user logged out!' })
 }
 
+const verifyEmail = async (req, res) => {
+  const { verificationToken, email } = req.body
+  const user = await User.findOne({ email })
+
+  if (!user) {
+    throw new CustomError.UnauthenticatedError('Verification failed')
+  }
+
+  if (user.verificationToken !== verificationToken) {
+    throw new CustomError.UnauthenticatedError('Verification failed')
+  }
+
+  user.isVerified = true
+  user.verified = Date.now()
+  user.verificationToken = ''
+
+  await user.save()
+
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: 'email verified', verificationToken, email })
+}
+
 module.exports = {
   register,
   login,
   logout,
+  verifyEmail,
 }
