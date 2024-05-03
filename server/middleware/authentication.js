91a7efcb6de130685d7e24756b22714afe6fac99
thenatural86 <philip.smalls86@gmail.com>
2024-05-03 12:21:58 -0300

    refactor auth middleware, refresh token

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -1,5 +1,6 @@
 const CustomError = require('../errors')
-const { isTokenValid } = require('../utils')
+const { isTokenValid, attachCookiesToResponse } = require('../utils')
+const Token = require('../models/Token')
 
 const authenticateUser = async (req, res, next) => {
   const { refreshToken, accessToken } = req.signedCookies
@@ -10,6 +11,26 @@ const authenticateUser = async (req, res, next) => {
       req.user = payload.user
       return next()
     }
+
+    const payload = isTokenValid(refreshToken)
+
+    const existingToken = await Token.findOne({
+      user: payload.user.userId,
+      refreshToken: payload.refreshToken,
+    })
+
+    if (!existingToken || !existingToken?.isValid) {
+      throw new CustomError.UnauthenticatedError('Authentication Invalid')
+    }
+
+    attachCookiesToResponse({
+      res,
+      user: payload.user,
+      refreshToken: existingToken.refreshToken,
+    })
+
+    req.user = payload.user
+    next()
   } catch (error) {
     throw new CustomError.UnauthenticatedError('Authentication Invalid')
   }
