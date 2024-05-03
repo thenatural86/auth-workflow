0b7a1cb2bec9c6078f87fbb58a4ce23e3cf1ae21
thenatural86 <philip.smalls86@gmail.com>
2024-05-03 12:06:51 -0300

    refactor auth middleware, access token

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -1,34 +1,32 @@
-const CustomError = require('../errors');
-const { isTokenValid } = require('../utils');
+const CustomError = require('../errors')
+const { isTokenValid } = require('../utils')
 
 const authenticateUser = async (req, res, next) => {
-  const token = req.signedCookies.token;
-
-  if (!token) {
-    throw new CustomError.UnauthenticatedError('Authentication Invalid');
-  }
+  const { refreshToken, accessToken } = req.signedCookies
 
   try {
-    const { name, userId, role } = isTokenValid({ token });
-    req.user = { name, userId, role };
-    next();
+    if (accessToken) {
+      const payload = isTokenValid(accessToken)
+      req.user = payload.user
+      return next()
+    }
   } catch (error) {
-    throw new CustomError.UnauthenticatedError('Authentication Invalid');
+    throw new CustomError.UnauthenticatedError('Authentication Invalid')
   }
-};
+}
 
 const authorizePermissions = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
       throw new CustomError.UnauthorizedError(
         'Unauthorized to access this route'
-      );
+      )
     }
-    next();
-  };
-};
+    next()
+  }
+}
 
 module.exports = {
   authenticateUser,
   authorizePermissions,
-};
+}
